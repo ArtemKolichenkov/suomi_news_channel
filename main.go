ad18c98115c12bab95ab8e2b18724f50a83f35a5
Vasiliy Aristov <v.aristov@sgmsoft.com>
2023-11-11 20:59:35 +0200

    Posting after approval

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-// 	"time"
+	"time"
 	"strconv"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
@@ -26,8 +26,8 @@ func main() {
 	var channelId int64
 	channelId, _ = strconv.ParseInt(os.Getenv("CHANNEL_ID"), 10, 64)
 
-// 	var adminChannelId int64
-// 	adminChannelId, _ = strconv.ParseInt(os.Getenv("ADMIN_CHANNEL_ID"), 10, 64)
+	var adminChannelId int64
+	adminChannelId, _ = strconv.ParseInt(os.Getenv("ADMIN_CHANNEL_ID"), 10, 64)
 
 	// Initialize Telegram bot
 	bot, err := tgbotapi.NewBotAPI(botToken)
@@ -40,43 +40,43 @@ func main() {
 	// Check each feed item
 	for _, item := range feed.Items {
 		// Send a message to the channel for approval
-// 		msg := tgbotapi.NewMessage(adminChannelId, fmt.Sprintf("Do you want to post this news?\n\n%s\n%s", item.Title, item.Link))
-// 		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
-// 			tgbotapi.NewInlineKeyboardRow(
-// 				tgbotapi.NewInlineKeyboardButtonData("Yes", "yes"),
-// 				tgbotapi.NewInlineKeyboardButtonData("No", "no"),
-// 			),
-// 		)
-//
-// 		// Send the message
-// 		message, err := bot.Send(msg)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-//
-// 		// Wait for a reply for a limited time
-// 		go func() {
-// 			time.Sleep(60 * time.Second) // Adjust as needed
-// 			// If no reply received, delete the message
-// 			deleteMsg := tgbotapi.NewDeleteMessage(adminChannelId, message.MessageID)
-// 			_, err := bot.Send(deleteMsg)
-// 			if err != nil {
-// 				log.Println(err)
-// 			}
-// 		}()
-//
-// 		// Wait for the user's reply
-// 		u := tgbotapi.NewUpdate(0)
-// 		u.Timeout = 60
-// 		updates, err := bot.GetUpdatesChan(u)
-//
-// 		for update := range updates {
-// 			if update.CallbackQuery != nil {
-// 				// User replied with "Yes" or "No"
-// 				replyText := ""
-// 				if update.CallbackQuery.Data == "yes" {
+		msg := tgbotapi.NewMessage(adminChannelId, fmt.Sprintf("Do you want to post this news?\n\n%s\n%s", item.Title, item.Link))
+		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+			tgbotapi.NewInlineKeyboardRow(
+				tgbotapi.NewInlineKeyboardButtonData("Yes", "yes"),
+				tgbotapi.NewInlineKeyboardButtonData("No", "no"),
+			),
+		)
+
+		// Send the message
+		message, err := bot.Send(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Wait for a reply for a limited time
+		go func() {
+			time.Sleep(60 * time.Second) // Adjust as needed
+			// If no reply received, delete the message
+			deleteMsg := tgbotapi.NewDeleteMessage(adminChannelId, message.MessageID)
+			_, err := bot.Send(deleteMsg)
+			if err != nil {
+				log.Println(err)
+			}
+		}()
+
+		// Wait for the user's reply
+		u := tgbotapi.NewUpdate(0)
+		u.Timeout = 60
+		updates, err := bot.GetUpdatesChan(u)
+
+		for update := range updates {
+			if update.CallbackQuery != nil {
+				// User replied with "Yes" or "No"
+				replyText := ""
+				if update.CallbackQuery.Data == "yes" {
 					// Post the news to the channel
-// 					replyText = fmt.Sprintf("Approved! Posting:\n\n%s\n%s", item.Title, item.Link)
+					replyText = fmt.Sprintf("Approved! Posting:\n\n%s\n%s", item.Title, item.Link)
 					postMessage := tgbotapi.NewMessage(channelId, fmt.Sprintf("%s\n%s", item.Title, item.Link))
 					postMessage.ParseMode = tgbotapi.ModeHTML
 					postMessage.DisableWebPagePreview = false
@@ -92,21 +92,23 @@ func main() {
 					if err != nil {
 						log.Println(err)
 					}
-// 				} else {
-// 					replyText = "News not posted."
-// 				}
-//
-// 				// Send a reply to the user
-// 				reply := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, replyText)
-// 				_, err := bot.Send(reply)
-// 				if err != nil {
-// 					log.Println(err)
-// 				}
+				} else {
+					replyText = "News not posted."
+				}
+
+				// Send a reply to the user
+				reply := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, replyText)
+				_, err := bot.Send(reply)
+				if err != nil {
+					log.Println(err)
+				}
 
 				// Stop waiting for updates
 				break
-// 			}
-// 		}
+			}
+		}
+
+		break
 	}
 
 	// Handle potential errors
